93692b02804826565abf8a8c80624b6a8d98e7b3
codecakes <akurnya@gmail.com>
2015-04-07 21:48:43 +0530

    using pip instead of exporting to Pythonpath common pkg

--- src/monitors/bootstrap-worker.sh
+++ src/monitors/bootstrap-worker.sh
@@ -6,6 +6,10 @@ echo "[BOOTSTRAP] Dumping Docker Environment:"
 echo "-------------------------------------------------"
 env
 
+echo "[BOOTSTRAP] Pip Installing common helper files:"
+pip install -U git+https://github.com/codecakes/cloudroutes_common_pkg.git@master
+echo "-------------------------------------------------"
+
 echo "[BOOTSTRAP] Generating config file"
 echo "-------------------------------------------------"
 cp /code/config/worker.yml.example /config/config.yml
