cc3a55bfb323be050609752d8b7994e75ab679e4
codecakes <akurnya@gmail.com>
2015-04-09 16:05:51 +0530

    added common package support to each container dirs via pip

--- src/monitors/bootstrap-broker.sh
+++ src/monitors/bootstrap-broker.sh
@@ -6,6 +6,10 @@ echo "[BOOTSTRAP] Dumping Docker Environment:"
 echo "-------------------------------------------------"
 env
 
+echo "[BOOTSTRAP] Installing Cloudroutes Common Package Support"
+pip install -e git+https://github.com/codecakes/cloudroutes_common_pkg.git@master#egg=cloudroutes_common_pkg
+echo "-------------------------------------------------"
+
 echo "[BOOTSTRAP] Generating config file"
 echo "-------------------------------------------------"
 cp /code/config/broker.yml.example /config/config.yml
