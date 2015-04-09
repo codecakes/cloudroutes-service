79737b9f4ba1df03ef63aaf7de363b98f3748e27
codecakes <akurnya@gmail.com>
2015-04-09 15:59:23 +0530

    updated to include common support in bootstrap

--- src/web/bootstrap.sh
+++ src/web/bootstrap.sh
@@ -7,7 +7,7 @@ echo "-------------------------------------------------"
 env
 
 echo "[BOOTSTRAP] Pip Installing common helper files:"
-pip install -U git+https://github.com/codecakes/cloudroutes_common_pkg.git@master
+pip install -e git+https://github.com/codecakes/cloudroutes_common_pkg.git@master#egg=cloudroutes_common_pkg
 echo "-------------------------------------------------"
 
 echo "[BOOTSTRAP] Generating config file"
